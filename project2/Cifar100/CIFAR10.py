68dfd48f46bcdc790da6460b94325803d2c8c486
HoliestCow <cbritt2@vols.utk.edu>
2017-11-07 13:53:32 -0800

    formatting for project2

--- project2/Cifar100/CIFAR10.py
+++ project2/Cifar100/CIFAR10.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 import urllib
 
 def get_proper_images(raw):
-    raw_float = np.array(raw, dtype=float) 
+    raw_float = np.array(raw, dtype=float)
     images = raw_float.reshape([-1, 3, 32, 32])
     images = images.transpose([0, 2, 3, 1])
     return images
@@ -36,17 +36,17 @@ def get_data():
     data3  = unpickle('../cifar-10-batches-py/data_batch_3')
     data4  = unpickle('../cifar-10-batches-py/data_batch_4')
     data5  = unpickle('../cifar-10-batches-py/data_batch_5')
-    
-    X = np.concatenate((get_proper_images(data1['data']), 
-                               get_proper_images(data2['data']), 
-                               get_proper_images(data3['data']), 
-                               get_proper_images(data4['data']), 
-                               get_proper_images(data5['data'])))
-    Y = np.concatenate((onehot_labels(data1['labels']), 
-                               onehot_labels(data2['labels']), 
-                               onehot_labels(data3['labels']), 
-                               onehot_labels(data4['labels']), 
-                               onehot_labels(data5['labels'])))
+
+    X = np.concatenate((get_proper_images(data1['data']),
+                        get_proper_images(data2['data']),
+                        get_proper_images(data3['data']),
+                        get_proper_images(data4['data']),
+                        get_proper_images(data5['data'])))
+    Y = np.concatenate((onehot_labels(data1['labels']),
+                        onehot_labels(data2['labels']),
+                        onehot_labels(data3['labels']),
+                        onehot_labels(data4['labels']),
+                        onehot_labels(data5['labels'])))
 
     X_test = get_proper_images(unpickle('../cifar-10-batches-py/test_batch')['data'])
     Y_test = onehot_labels(unpickle('../cifar-10-batches-py/test_batch')['labels'])
@@ -55,7 +55,7 @@ def get_data():
     if data_norm:
         img_prep.add_featurewise_zero_center()
         img_prep.add_featurewise_stdnorm()
-    
+
     img_aug = ImageAugmentation()
     if data_augmentation:
         img_aug.add_random_flip_leftright()
@@ -76,18 +76,18 @@ def my_model(img_prep, img_aug):
 #     activation_function = 'sigmoid'
     objective_function = 'categorical_crossentropy'
 #     objective_function = 'mean_square'
-    
+
     network = input_data(shape=[None, 32, 32, 3],
                          data_preprocessing=img_prep,
                          data_augmentation=img_aug)
     network = conv_2d(network, 32, 3, strides=1, padding='same', activation=activation_function,
                       bias=True, bias_init='zeros', weights_init=initializer)
-    network = max_pool_2d(network, 2 , strides=None, padding='same')
+    network = max_pool_2d(network, 2, strides=None, padding='same')
     network = conv_2d(network, 64, 3, strides=1, padding='same', activation=activation_function,
                       bias=True, bias_init='zeros', weights_init=initializer)
-    network = conv_2d(network, 64, 3 , strides=1, padding='same', activation=activation_function,
+    network = conv_2d(network, 64, 3, strides=1, padding='same', activation=activation_function,
                       bias=True, bias_init='zeros', weights_init=initializer)
-    network = max_pool_2d(network, 2 , strides=None, padding='same')
+    network = max_pool_2d(network, 2, strides=None, padding='same')
     network = fully_connected(network, 512, activation=activation_function)
     network = dropout(network, dropout_probability)
     network = fully_connected(network, 10, activation='softmax')
@@ -105,8 +105,8 @@ def main():
 #         with tf.contrib.framework.arg_scope([tflearn.variables.variable], device='/cpu:0'):
     model = my_model(img_prep, img_aug)
     network = DNN(model)
-    network.fit(x, y, n_epoch=100, shuffle=True, validation_set=(x_test, y_test), show_metric=True, batch_size=100 , run_id='aa2')
-
+    network.fit(x, y, n_epoch=100, shuffle=True, validation_set=(x_test, y_test), show_metric=True,
+                batch_size=100, run_id='aa2')
     return
 
 main()
