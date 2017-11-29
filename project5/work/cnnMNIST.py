6ab808b0cbbb2575da66a6cc534e0fcb3e27a019
HoliestCow <utkcbritt2@gmail.com>
2017-11-29 16:13:00 -0500

    it works my dude

--- project5/work/cnnMNIST.py
+++ project5/work/cnnMNIST.py
@@ -2,12 +2,12 @@
 
 import tensorflow as tf
 import numpy as np
-from random import shuffle
 import time
 import h5py
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import itertools
+from copy import deepcopy
 
 # from tensorflow.examples.tutorials.mnist import input_data
 # mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -15,7 +15,7 @@ import itertools
 class cnnMNIST(object):
     def __init__(self):
         self.lr = 1e-3
-        self.epochs = 100
+        self.epochs = 400
         self.build_graph()
 
     def onehot_labels(self, labels):
@@ -36,11 +36,11 @@ class cnnMNIST(object):
 
         f = h5py.File('naive_dataset_large.h5', 'r')
         g = f['training']
-        X = g['spectra']
+        X = np.array(g['spectra'])
         Y = self.onehot_labels(np.array(g['labels'], dtype=np.int32))
 
         g = f['testing']
-        X_test = g['spectra']
+        X_test = np.array(g['spectra'])
         Y_test = self.onehot_labels(np.array(g['labels'], dtype=np.int32))
 
         # img_prep = ImagePreprocessing()
@@ -75,10 +75,10 @@ class cnnMNIST(object):
 
         x_image = self.hack_1dreshape(self.x)
         # define conv-layer variables
-        W_conv1 = self.weight_variable([1, 5, 1, 32])    # first conv-layer has 32 kernels, size=5
-        b_conv1 = self.bias_variable([32])
-        W_conv2 = self.weight_variable([1, 5, 32, 64])
-        b_conv2 = self.bias_variable([64])
+        W_conv1 = self.weight_variable([1, 5, 1, 16])    # first conv-layer has 32 kernels, size=5
+        b_conv1 = self.bias_variable([16])
+        W_conv2 = self.weight_variable([1, 5, 16, 32])
+        b_conv2 = self.bias_variable([32])
 
         # x_image = tf.reshape(self.x, [-1, 28, 28, 1])
         h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
@@ -87,10 +87,10 @@ class cnnMNIST(object):
         h_pool2 = self.max_pool_2x2(h_conv2)
 
         # densely/fully connected layer
-        W_fc1 = self.weight_variable([256 * 64, 1024])
-        b_fc1 = self.bias_variable([1024])
+        W_fc1 = self.weight_variable([256 * 32, 256])
+        b_fc1 = self.bias_variable([256])
 
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 256 * 64])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 256 * 32])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
         # dropout regularization
@@ -98,7 +98,7 @@ class cnnMNIST(object):
         h_fc1_drop = tf.nn.dropout(h_fc1, self.keep_prob)
 
         # linear classifier
-        W_fc2 = self.weight_variable([1024, 7])
+        W_fc2 = self.weight_variable([256, 7])
         b_fc2 = self.bias_variable([7])
 
         self.y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
@@ -106,18 +106,11 @@ class cnnMNIST(object):
         self.train_step = tf.train.AdamOptimizer(self.lr).minimize(cross_entropy)
 
     def shuffle(self):
-        # Given list1 and list2
-        x_shuf = np.zeros((self.x_train.shape[0], self.x_train.shape[1]))
-        y_shuf = np.zeros((self.y_train.shape[0], self.y_train.shape[1]))
-        index_shuf = list(range(x_shuf.shape[0]))
-        shuffle(index_shuf)
-        counter = 0
-        for i in index_shuf:
-            x_shuf[counter, :] = self.x_train[i, :]
-            y_shuf[counter, :] = self.y_train[i, :]
-            counter += 1
-        self.x_train = x_shuf
-        self.y_train = y_shuf
+        rng_state = np.random.get_state()
+        np.random.set_state(rng_state)
+        np.random.shuffle(self.x_train)
+        np.random.set_state(rng_state)
+        np.random.shuffle(self.y_train)
         return
 
     def train(self):
@@ -127,13 +120,13 @@ class cnnMNIST(object):
         self.eval() # creating evaluation
         for i in range(self.epochs):
             # batch = mnist.train.next_batch(50)
-            x_generator = self.batch(self.x_train, n=10)
-            y_generator = self.batch(self.y_train, n=10)
+            x_generator = self.batch(self.x_train, n=32)
+            y_generator = self.batch(self.y_train, n=32)
             # print(batch[0].shape)
             # print(batch[1].shape)
-            if i % 100 == 0 and i != 0:
-                train_acc = self.sess.run(self.accuracy,feed_dict={self.x: self.x_test,
-                                                                   self.y_: self.y_test,
+            if i % 10 == 0 and i != 0:
+                train_acc = self.sess.run(self.accuracy,feed_dict={self.x: self.x_test[:1000, :],
+                    self.y_: self.y_test[:1000, :],
                                                                    self.keep_prob: 1.0})
                 print('step %d, training accuracy %g' % (i, train_acc))
             self.sess.run([self.train_step], feed_dict={self.x: next(x_generator),
@@ -150,7 +143,7 @@ class cnnMNIST(object):
     def test_eval(self):
         self.eval()
         test_acc = self.sess.run(self.accuracy, feed_dict={
-                self.x: self.x_test, self.y_: self.y_test, self.keep_prob: 1.0})
+            self.x: self.x_test[:1000, :], self.y_: self.y_test[:1000, :], self.keep_prob: 1.0})
         print('test accuracy %g' % test_acc)
 
     def weight_variable(self, shape):
@@ -176,7 +169,7 @@ class cnnMNIST(object):
 
     def get_label_predictions(self):
         predictions = self.sess.run(self.prediction,
-                                    feed_dict={self.x: self.x_test,
+                feed_dict={self.x: self.x_test[:1000, :],
                                                self.keep_prob: 1.0})
         return predictions
 
@@ -232,17 +225,11 @@ def main():
 
     predictions = cnn.get_label_predictions()
 
-    # predictions_decode = cnn.onenothot_labels(predictions)
-    # labels_decode = cnn.onenothot_labels(cnn.y_test)
-
     predictions_decode = predictions
-    labels_decode = cnn.y_test
+    labels_decode = cnn.onenothot_labels(cnn.y_test)
 
-    np.save(predictions_decode, 'predictions.npy')
-
-    labels_decode = cnn.onenothot_labels(labels_decode)
-
-    np.save(labels_decode, 'ground_truth.npy')
+    np.save('predictions.npy', predictions_decode)
+    np.save('ground_truth.npy', labels_decode[:1000])
 
     return
 
