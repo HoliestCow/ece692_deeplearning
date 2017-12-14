323e20c5eef0131db7e696d7694d0d8e733b8b8e
HoliestCow <cbritt2@vols.utk.edu>
2017-12-14 15:10:34 -0800

    seq changes

--- project5/cnn/cnnseqDETandSID.py
+++ project5/cnn/cnnseqDETandSID.py
@@ -19,14 +19,14 @@ import os.path
 class cnnMNIST(object):
     def __init__(self):
         self.lr = 1e-3
-        self.epochs = 500
-        self.runname = 'cnndetandsidweight_{}'.format(self.epochs)
+        self.epochs = 100
+        self.runname = 'cnnseqdetandsid_{}'.format(self.epochs)
         self.build_graph()
 
     def onehot_labels(self, labels):
-        out = np.zeros((labels.shape[0], 7))
-        for i in range(labels.shape[0]):
-            out[i, :] = np.eye(7)[labels[i]]
+        out = np.zeros((len(labels), 7))
+        for i in range(len(labels)):
+            out[i, :] = np.eye(7)[int(labels[i])]
         return out
 
     def onenothot_labels(self, labels):
@@ -38,47 +38,69 @@ class cnnMNIST(object):
     def get_data(self):
         # data_norm = True
         # data_augmentation = False
+        try:
+            f = h5py.File('./sequential_dataset_balanced.h5', 'r')
+        except:
+            f = h5py.File('/home/holiestcow/Documents/2017_fall/ne697_hayward/lecture/datacompetition/sequential_dataset_balanced.h5', 'r')
 
-        f = h5py.File('naive_dataset.h5', 'r')
-        g = f['training']
-        X = np.array(g['spectra'])
-        Y = self.onehot_labels(np.array(g['labels'], dtype=np.int32))
-
-        g = f['testing']
-        X_test = np.array(g['spectra'])
-        Y_test = self.onehot_labels(np.array(g['labels'], dtype=np.int32))
+        X = f['train']
+        X_test = f['test']
 
         self.x_train = X
-        self.y_train = Y
-
         self.x_test = X_test
-        self.y_test = Y_test
-
-        f.close()
+        # NOTE: always use the keylist to get data
+        self.data_keylist = list(X.keys())
 
         return
 
-    def batch(self, iterable, n=1, shuffle=True):
+    def batch(self, iterable, n=1, shuffle=True, small_test=True, usethesekeys = None, shortset=False):
         if shuffle:
             self.shuffle()
+        if usethesekeys is None:
+            keylist = self.data_keylist
+        else:
+            keylist = usethesekeys
+            if shortset:
+                keylist = usethesekeys[:100]
+
+
         # l = len(iterable)
-        l = iterable.shape[0]
-        for ndx in range(0, l, n):
-            data = iterable[ndx:min(ndx + n, l), :]
-            # normalization = np.linalg.norm(data, 1, axis=1)
-            # for j in range(data.shape[0]):
-            #     data[j, :] = np.divide(data[j, :], normalization[j])
-            yield data
+        for i in range(len(keylist)):
+            x = np.array(iterable[keylist[i]]['measured_spectra'])
+            y = np.array(iterable[keylist[i]]['labels'])
+            # NOTE: For using cnnfeatures sequential dataset
+            # x = np.array(iterable[keylist[i]]['features'])
+            # y = np.array(iterable[keylist[i]]['labels'])
+            mask = y >= 0.5
+            # y[mask] = 1
+            z = np.ones((y.shape[0],))
+            # z[mask] = 6.0
+            y = self.onehot_labels(y)
+            self.current_batch_length = x.shape[0]
+            yield x, y, z
+
+            # for j in range(self.current_batch_length):
+            #     stuff = y[j,:]
+            #     stuff = stuff.reshape((1, 7))
+            #     yield x[j, :], stuff, z[j]
 
     def validation_batcher(self):
-        f = h5py.File('./naive_dataset.h5', 'r')
-        g = f['validation']
+        try:
+            f = h5py.File('sequential_dataset_balanced.h5', 'r')
+        except:
+            f = h5py.File('/home/holiestcow/Documents/2017_fall/ne697_hayward/lecture/datacompetition/sequential_dataset_balanced.h5', 'r')
+        g = f['validate']
         samplelist = list(g.keys())
+        # samplelist = samplelist[:10]
 
         for i in range(len(samplelist)):
-            data = g[samplelist[i]]
+            self.current_sample_name = samplelist[i]
+            data = np.array(g[samplelist[i]])
+            self.current_batch_length = data.shape[0]
             yield data
-
+            # for j in range(self.current_batch_length):
+            #     current_x = np.squeeze(data[j, :, :])
+            #     yield current_x
 
     def build_graph(self):
 
@@ -87,21 +109,25 @@ class cnnMNIST(object):
 
         final_hidden_nodes = 1024
 
-        self.x = tf.placeholder(tf.float32, shape=[None, 1024])
+        self.x = tf.placeholder(tf.float32, shape=[None, 15, 1024])
         self.y_ = tf.placeholder(tf.float32, shape=[None, 7])
 
         x_image = self.hack_1dreshape(self.x)
         # define conv-layer variables
-        W_conv1 = self.weight_variable([1, 3, 1, feature_map1])    # first conv-layer has 32 kernels, size=5
-        b_conv1 = self.bias_variable([feature_map1])
-        W_conv2 = self.weight_variable([1, 3, feature_map1, feature_map2])
-        b_conv2 = self.bias_variable([feature_map2])
+        # Try 17, 3??
+        print(x_image)
+        W_conv1 = self.weight_variable([1, 9, 1, feature_map2])
+        b_conv1 = self.bias_variable([feature_map2])
+        W_conv2 = self.weight_variable([3, 1, feature_map2, feature_map1])
+        b_conv2 = self.bias_variable([feature_map1])
 
         # x_image = tf.reshape(self.x, [-1, 28, 28, 1])
         h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
-        h_pool1 = self.max_pool_2x2(h_conv1)
+        # h_pool1 = self.max_pool_2x2(h_conv1)
+        h_pool1 = self.max_pool_spectra(h_conv1)
         h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
-        h_pool2 = self.max_pool_2x2(h_conv2)
+        # h_pool2 = self.max_pool_2x2(h_conv2)
+        h_pool2 = self.max_pool_time(h_conv2)
 
         # W_conv3 = self.weight_variable([1, 3, feature_map2, feature_map3])
         # b_conv3 = self.bias_variable([feature_map3])
@@ -114,10 +140,12 @@ class cnnMNIST(object):
         # h_pool4 = self.max_pool_2x2(h_conv4)
 
         # densely/fully connected layer
-        W_fc1 = self.weight_variable([256 * feature_map2, final_hidden_nodes])
+        print(h_pool2.shape)
+        sizing_variable = 512 * 8
+        W_fc1 = self.weight_variable([sizing_variable * feature_map1, final_hidden_nodes])
         b_fc1 = self.bias_variable([final_hidden_nodes])
 
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 256 * feature_map2])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, sizing_variable * feature_map1])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
         # W_fc1 = self.weight_variable([64 * feature_map4, final_hidden_nodes])
@@ -134,20 +162,18 @@ class cnnMNIST(object):
         W_fc2 = self.weight_variable([final_hidden_nodes, 7])
         b_fc2 = self.bias_variable([7])
 
-        y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+        self.y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
         # Now I have to weight to logits
-        class_weights = tf.constant([0.1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
-        self.y_conv = tf.multiply(y_conv, class_weights)
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y_conv))
-        self.train_step = tf.train.AdamOptimizer(self.lr).minimize(cross_entropy)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y_conv))
+        self.train_step = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
 
     def shuffle(self):
-        rng_state = np.random.get_state()
-        np.random.set_state(rng_state)
-        np.random.shuffle(self.x_train)
-        np.random.set_state(rng_state)
-        np.random.shuffle(self.y_train)
+        # rng_state = np.random.get_state()
+        # np.random.set_state(rng_state)
+        np.random.shuffle(self.data_keylist)
+        # np.random.set_state(rng_state)
+        # np.random.shuffle(self.y_train)
         return
 
     def train(self):
@@ -155,22 +181,35 @@ class cnnMNIST(object):
         init = tf.global_variables_initializer()
         self.sess.run(init)
         self.eval() # creating evaluation
+        a = time.time()
         for i in range(self.epochs):
-            # batch = mnist.train.next_batch(50)
-            x_generator = self.batch(self.x_train, n=128)
-            y_generator = self.batch(self.y_train, n=128)
-            # print(batch[0].shape)
-            # print(batch[1].shape)
             if i % 10 == 0 and i != 0:
-                test_acc = self.sess.run(self.accuracy,feed_dict={self.x: self.x_test[:1000, :],
-                    self.y_: self.y_test[:1000, :],
-                                                                   self.keep_prob: 1.0})
-                train_acc = self.sess.run(self.accuracy, feed_dict={self.x: current_x,
-                                                                   self.y_: current_y,
-                                                                   self.keep_prob: 1.0})
-                print('step %d, training accuracy %g, testing accuracy %g' % (i, train_acc, test_acc))
-            current_x = next(x_generator)
-            current_y = next(y_generator)
+                counter = 0
+                sum_acc = 0
+                sum_loss = 0
+                hits = 0
+                meh = 0
+                x_generator_test = self.batch(self.x_test,
+                                              usethesekeys=list(self.x_test.keys()), shortset=True)
+                for j, k, z in x_generator_test:
+                    # NOTE: quick and dirty preprocessing once again
+                    # feedme = j / j.sum(axis=-1, keepdims=True)
+                    feedme = j
+                    accuracy, train_loss, prediction = self.sess.run(
+                        [self.accuracy, self.loss, self.y_conv],
+                        feed_dict={self.x: feedme,
+                            self.y_: k,
+                            self.keep_prob: 1.0})
+                            #    self.weights: z})
+                    sum_loss += np.sum(train_loss)
+                    hits += np.sum(prediction)
+                    sum_acc += accuracy
+                    counter += feedme.shape[0]
+                    meh += 1
+                b = time.time()
+                print('step {}:\navg acc {}\navg loss {}\ntotalhits {}\ntime elapsed: {} s'.format(i, sum_acc / meh, sum_loss / counter, hits, b-a))
+            x_generator = self.batch(self.x_train, n=128)
+            current_x, current_y, current_z = next(x_generator)
             self.sess.run([self.train_step], feed_dict={self.x: current_x,
                                                         self.y_: current_y,
                                                         self.keep_prob: 0.50})
@@ -205,16 +244,23 @@ class cnnMNIST(object):
 
     def hack_1dreshape(self, x):
         # expand its dimensionality to fit into conv2d
-        tensor_expand = tf.expand_dims(x, 1)
-        tensor_expand = tf.expand_dims(tensor_expand, -1)
+        # tensor_expand = tf.expand_dims(x, 1)
+        tensor_expand = tf.expand_dims(x, -1)
         return tensor_expand
 
     def conv2d(self, x, W):
         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-    def max_pool_2x2(self, x):
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                                strides=[1, 2, 2, 1], padding='SAME')
+    # def max_pool_2x2(self, x):
+    #     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+    #                             strides=[1, 2, 2, 1], padding='SAME')
+    def max_pool_spectra(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 1, 2, 1],
+                              strides=[1, 1, 2, 1], padding='SAME')
+
+    def max_pool_time(self, x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 1, 1],
+                              strides=[1, 2, 1, 1], padding='SAME')
 
     def get_label_predictions(self):
         x_batcher = self.batch(self.x_test, n=1000, shuffle=False)
@@ -228,42 +274,6 @@ class cnnMNIST(object):
             temp_predictions = temp_predictions.reshape((temp_predictions.shape[0], 1))
             predictions = np.vstack((predictions, temp_predictions))
         return predictions
-
-
-# def plot_confusion_matrix(cm, classes,
-#                           normalize=False,
-#                           title='Confusion matrix',
-#                           cmap=plt.cm.Blues):
-#     """
-#     This function prints and plots the confusion matrix.
-#     Normalization can be applied by setting `normalize=True`.
-#     """
-#     if normalize:
-#         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-#         print("Normalized confusion matrix")
-#     else:
-#         print('Confusion matrix, without normalization')
-#
-#     print(cm)
-#
-#     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-#     plt.title(title)
-#     plt.colorbar()
-#     tick_marks = np.arange(len(classes))
-#     plt.xticks(tick_marks, classes, rotation=45)
-#     plt.yticks(tick_marks, classes)
-#
-#     fmt = '.2f' if normalize else 'd'
-#     thresh = cm.max() / 2.
-#     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-#         plt.text(j, i, format(cm[i, j], fmt),
-#                  horizontalalignment="center",
-#                  color="white" if cm[i, j] > thresh else "black")
-#
-#     plt.tight_layout()
-#     plt.ylabel('True label')
-#     plt.xlabel('Predicted label')
-
 
 def main():
     cnn = cnnMNIST()
@@ -279,7 +289,7 @@ def main():
     cnn.train()
     b = time.time()
     print('Training time: {} s'.format(b-a))
-    cnn.test_eval()
+    # cnn.test_eval()
 
     predictions = cnn.get_label_predictions()
 
@@ -289,7 +299,7 @@ def main():
     np.save('{}_predictions.npy'.format(cnn.runname), predictions_decode)
     np.save('{}_ground_truth.npy'.format(cnn.runname), labels_decode)
 
-    answers = open('approach1_answers.csv', 'w')
+    answers = open('approach1c_answers.csv', 'w')
     answers.write('RunID,SourceID,SourceTime,Comment\n')
     # counter = 0
     for sample in validation_data:
